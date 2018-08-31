4a08f03401309f5a95d166c57c004d8fc84f8a83
MRH <mariarhennig@gmail.com>
2018-08-31 14:47:18 +0100

    Added images

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
     });
 });
 
-
 function showme(){
     var item = document.getElementById("result");
     if (item) {
@@ -125,9 +124,12 @@ function coffeecall(weekday){
 }
 
 function list_setup(results_list,weekday){
+    
+    // order results by rating
     results_list.sort(function(a,b){
         return b.rating-a.rating;
     })
+
     for(i=0; i < results_list.length; i++){
         // name 
         var name = results_list[i].name;
@@ -180,25 +182,176 @@ function list_setup(results_list,weekday){
 
         // google maps URL
         var url = "https://www.google.com/maps/search/"+name+"+"+address+"+Cork/";
+
+        // images
+        var image_list = [
+            {
+                name: "Portafilter",
+                id: 0,
+                image: "./images/cafes/portafilter.jpg"
+            },
+            {
+                name: "Alchemy by The Lough",
+                id: 1,
+                image: "./images/cafes/alchemylough.jpg"
+            },
+            {
+                name: "Three Fools Coffee",
+                id: 2,
+                image: "./images/cafes/threefools.jpg"
+            },
+            {
+                name: "Filter",
+                id: 3,
+                image: "./images/cafes/filter.jpg"
+            },
+            {
+                name: "Myo Cafe",
+                id: 4,
+                image: "./images/cafes/myo.jpg"
+            },
+            {
+                name: "Cork Coffee Roasters, French Church",
+                id: 5,
+                image: "./images/cafes/ccrfrench.jpg"
+            },
+            {
+                name: "Cafe Depeche",
+                id: 6,
+                image: "./images/cafes/depeche.png"
+            },
+            {
+                name: "Soma Coffee Company",
+                id: 7,
+                image: "./images/cafes/soma.jpg"
+            },
+            {
+                name: "Alchemy Cork",
+                id: 8,
+                image: "./images/cafes/alchemy.jpg"
+            },
+            {
+                name: "Warren Allen Coffee",
+                id: 9,
+                image: "./images/cafes/warrenallen.png"
+            },
+            {
+                name: "Priory Coffee Co.",
+                id: 10,
+                image: "./images/cafes/priory.jpg"
+            },
+            {
+                name: "The Kino",
+                id: 11,
+                image: "./images/cafes/kino.jpg"
+            },
+            {
+                name: "Cork Coffee Roasters, Bridge Street",
+                id: 12,
+                image: "./images/cafes/ccrbridge.jpg"
+            },
+            {
+                name: "Nectar Coffee",
+                id: 13,
+                image: "./images/cafes/nectar.jpg"
+            },
+            {
+                name: "Dukes Coffee Company",
+                id: 14,
+                image: "./images/cafes/dukes.png"
+            },
+            {
+                name: "The Bookshelf Coffee House",
+                id: 15,
+                image: "./images/cafes/bookshelf.png"
+            },
+            {
+                name: "Union Grind Espresso Bar",
+                id: 16,
+                image: "./images/cafes/union.png"
+            },
+            {
+                name: "pop* coffee",
+                id: 17,
+                image: "./images/cafes/popcoffee.png"
+            },
+            {
+                name: "Café Eco",
+                id: 18,
+                image: "./images/cafes/eco.jpg"
+            }
+        ]
+        console.log(image_list[0].url);
+
+        if (name == "Portafilter"){
+            var img_url = image_list[0].image;
+        } else if (name == "pop* coffee"){
+            var img_url = image_list[17].image;
+        } else if (name == "Alchemy by The Lough"){
+            var img_url = image_list[1].image;
+        } else if (name == "Three Fools Coffee"){
+            var img_url = image_list[2].image;
+        } else if (name == "Filter"){
+            var img_url = image_list[3].image;
+        } else if (name == "Myo Cafe"){
+            var img_url = image_list[4].image;
+        } else if (name == "Cork Coffee Roasters" && address == "2 French Church Street"){
+            var img_url = image_list[5].image;
+        } else if (name == "Cafe Depeche"){
+            var img_url = image_list[6].image;
+        } else if (name == "Soma Coffee Company"){
+            var img_url = image_list[7].image;
+        } else if (name == "Alchemy Cork"){
+            var img_url = image_list[8].image;
+        } else if (name == "Warren Allen Coffee"){
+            var img_url = image_list[9].image;
+        } else if (name == "Priory Coffee Co."){
+            var img_url = image_list[10].image;
+        } else if (name == "The Kino"){
+            var img_url = image_list[11].image;
+        } else if (name == "Cork Coffee Roasters" && address == "2 Bridge Street"){
+            var img_url = image_list[12].image;
+        } else if (name == "Nectar Coffee"){
+            var img_url = image_list[13].image;
+        } else if (name == "Dukes Coffee Company"){
+            var img_url = image_list[14].image;
+        } else if (name == "The Bookshelf Coffee House"){
+            var img_url = image_list[15].image;
+        } else if (name == "Union Grind Espresso Bar"){
+            var img_url = image_list[16].image;
+        } else if (name == "Café Eco"){
+            var img_url = image_list[18].image;
+        }
         
 
-        console.log(name,rating, address,star_rating,close,url);
-        create_table(name,star_rating,address,close,url,results_list);
+        // console.log(name,rating, address,star_rating,close,url);
+        create_table(name,star_rating,address,close,url,results_list, img_url);
     }
 }
 
-function create_table(name,star_rating,address,close,url,results_list){
+function create_table(name,star_rating,address,close,url,results_list, img_url){
 
     var index = results_list.indexOf(results_list[i]);
 
     var tr = document.createElement("tr");
     document.getElementById("coffee-table").appendChild(tr);
 
+    var td_image = document.createElement("td");
+    // td_image.id = "td_image-"+index;
+    tr.appendChild(td_image);
+    var image_box = document.createElement("div");
+    // image_box.id = "td_image-"+index;
+    image_box.style.backgroundImage = "url("+img_url+")";
+    image_box.className = "imagebox";
+    td_image.appendChild(image_box);
+
+   /*  document.getElementById("td_image-"+index).innerHTML = "<img src='"+img_url+"' height='100px' width='100px'>"; */
+
     var td_name = document.createElement("td");
     td_name.id = "td_name-"+index;
     td_name.className ="namelist";
     tr.appendChild(td_name);
-    document.getElementById("td_name-"+index).innerHTML= "<a href='"+url+"' target=_blank>"+name+"</a>"
+    document.getElementById("td_name-"+index).innerHTML= "<a href='"+url+"' target=_blank>"+name+"</a>";
 
     var td_address = document.createElement("td");
     var node_address = document.createTextNode(address);
